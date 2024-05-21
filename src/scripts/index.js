edfd76fb1e017d4004d6893747335f3b0c7d0ba3
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-05-22 02:51:31 +0700

    setupindexjs

---
+++ src/scripts/index.js
@@ -0,0 +1,22 @@
+import 'regenerator-runtime';
+import '../styles/home.scss';
+// eslint-disable-next-line import/extensions
+import App from './views/app';
+import swRegister from './utils/sw-register';
+
+const app = new App({
+  button: document.querySelector('#menuToggle'),
+  drawer: document.querySelector('#drawer-navigasi'),
+  content: document.querySelector('main'),
+  skip: document.querySelector('#skip'),
+  mainkonten: document.querySelector('main'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', async () => {
+  app.renderPage();
+  await swRegister();
+});
