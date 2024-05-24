7451e9d5b1844badb535c901a580be5e2b74c5de
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-05-24 07:05:43 +0700

    lint

--- src/scripts/utils/drawer-initiator.js
+++ src/scripts/utils/drawer-initiator.js
@@ -1,37 +1,36 @@
 const Inisiatordrawer = {
-    init({
-      button, drawer, content, skip,
-    }) {
-      button.addEventListener('click', (event) => {
-        this._drawertoggle(event, drawer);
-      });
-  
-      content.addEventListener('click', (event) => {
-        this._drawertutup(event, drawer);
-      });
-  
-      skip.addEventListener('click', () => {
-        const mainElement = document.getElementById('main');
-        if (mainElement) {
-          mainElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
-          const firstFocusableElement = mainElement.querySelector('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
-          if (firstFocusableElement) {
-            firstFocusableElement.focus();
-          }
+  init({
+    button, drawer, content, skip,
+  }) {
+    button.addEventListener('click', (event) => {
+      this._drawertoggle(event, drawer);
+    });
+
+    content.addEventListener('click', (event) => {
+      this._drawertutup(event, drawer);
+    });
+
+    skip.addEventListener('click', () => {
+      const mainElement = document.getElementById('main');
+      if (mainElement) {
+        mainElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        const firstFocusableElement = mainElement.querySelector('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+        if (firstFocusableElement) {
+          firstFocusableElement.focus();
         }
-      });
-    },
-  
-    _drawertoggle(event, drawer) {
-      event.stopPropagation();
-      drawer.classList.toggle('open');
-    },
-  
-    _drawertutup(event, drawer) {
-      event.stopPropagation();
-      drawer.classList.remove('open');
-    },
-  };
-  
-  export default Inisiatordrawer;
-  
+      }
+    });
+  },
+
+  _drawertoggle(event, drawer) {
+    event.stopPropagation();
+    drawer.classList.toggle('open');
+  },
+
+  _drawertutup(event, drawer) {
+    event.stopPropagation();
+    drawer.classList.remove('open');
+  },
+};
+
+export default Inisiatordrawer;
