d01dca90e2cbada6446680f0a44b62543e10c35a
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-05-25 22:04:04 +0700

    buat creator template

---
+++ src/scripts/views/templates/template-creator.js
@@ -0,0 +1,60 @@
+// import CONFIG from '../../globals/configure';
+
+const daftarpopuler = (populer) => `
+<article>
+  <img src="./images/sinjay.jpg" alt="gambar wisata kuliner">
+  <p class="lok">Bangkalan</p>
+  <h3><a href="#/detail/${populer.id}">Bebek Sinjai </a></h3>
+  <p>Buka 08.00-21.00 <span>&#9733;5</span></p>
+  <section>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+  <div><button>Detail<span>→</span></button></div>
+  </section> 
+</article>`;
+const daftartestimoni = (testimoni) => `
+<article>
+  <img src="./images/sinjay.jpg" alt="gambar wisata kuliner">
+  <h3><a href="#/detail/${testimoni.id}">Bebek Sinjai </a></h3>
+  <section>
+    <h4>Yondaime Hokage</h4>
+    <p>&#9733;5</p>
+    <span>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+    </span>
+  </section> 
+</article>`;
+const allkuliner = (kuliner) => `
+<article>
+  <img src="./images/sinjay.jpg" alt="gambar wisata kuliner">
+  <p class="lok">Bangkalan</p>
+  <h3><a href="#/detail/${kuliner.id}">Bebek Sinjai </a></h3>
+  <p>Buka 08.00-21.00 <span>&#9733;5</span></p>
+  <section>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
+  <div><button>Detail<span>→</span></button></div>
+  </section> 
+</article>`;
+
+const searchkuliner = `
+<form id="searchForm" class="searching">
+  <div class="input">
+    <input type="text" id="searchInput" name="query" placeholder="Search anything...." required>
+    <button type="submit" id="searchButton">⌕</button>
+  </div>
+  <div class="dropdown">
+  <button class="dropbtn">⌨</button>
+  <div class="dropdown-content">
+      <a href="#">Bangkalan</a>
+      <a href="#">Sampang</a>
+      <a href="#">Pamekasan</a>
+      <a href="#">Sumenep</a>
+  </div>
+</div>
+</form>`;
+
+export {
+  daftarpopuler,
+  daftartestimoni,
+  allkuliner,
+  searchkuliner,
+};
