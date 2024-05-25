a27c598731ee4fa0797f8fbe599795ed0a6cd8cc
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-05-25 22:00:55 +0700

    routes dan perbaikan index html menambahkan footer dari jahfal

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,8 +1,10 @@
 import Pagehome from '../views/pages/pagehome';
+import Pagekuliner from '../views/pages/pagekuliner';
 
 const routes = {
   '/': Pagehome, // sebagai halaman utama/ default
   '/pagehome': Pagehome,
+  '/pagekuliner': Pagekuliner,
 };
 
 export default routes;
