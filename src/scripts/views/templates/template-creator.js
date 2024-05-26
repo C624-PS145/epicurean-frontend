992009c8d43d9e89e4bc3299524e8087082af9e9
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-05-26 23:20:51 +0700

    about creator template

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -8,7 +8,11 @@ const daftarpopuler = (populer) => `
   <p>Buka 08.00-21.00 <span>&#9733;5</span></p>
   <section>
   <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy</p>
-  <div><button>Detail<span>→</span></button></div>
+  <div><button>
+  <a href="#/detail/${populer.id}">  Detail<span>→</span></a>
+  </button></div>
+
+
   </section> 
 </article>`;
 const daftartestimoni = (testimoni) => `
@@ -52,9 +56,70 @@ const searchkuliner = `
 </div>
 </form>`;
 
+const aboutplate = `
+<div class="developer">
+    <h2>Developer.</h2>
+    <div class="developer-container">
+        <div class="anggota-developer">
+            <img src="./images/yohan.jpg" alt="Profil Yohan Permana">
+            <h3>Yohan Permana</h3>
+            <p>Front-End 1 Web Dev</p>
+            <div class="github">
+                <img src="./images/github.png" alt="Github">
+                <p>YohanpermanaRepository</p>
+            </div>
+        </div>
+        <div class="anggota-developer">
+            <img src="./images/jahfal.jpg" alt="Profil Jahfal Azzuhri Subroto">
+            <h3>Jahfal Azzuhri S</h3>
+            <p>Front-End 2 Web Dev</p>
+            <div class="github">
+            <img src="./images/github.png" alt="Github">
+                <p>jahfal1987</p>
+            </div>
+        </div>
+        <div class="anggota-developer">
+            <img src="./images/hikmal.jpg" alt="Profil M Nur Hikmal A">
+            <h3>M Nur Hikmal A</h3>
+            <p>Back End Web Dev</p>
+            <div class="github">
+            <img src="./images/github.png" alt="Github">
+                <p>HikmalRepository</p>
+            </div>
+        </div>
+    </div> 
+</div>
+
+
+<div class="visi-misi">
+    <h2>Visi Misi</h2>
+    <div class="visi-misi-container">
+        <div class="visi">
+            <h3>Visi</h3>
+            <ul>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+            </ul>
+        </div>
+        <div class="misi">
+            <h3>Misi</h3>
+            <ul>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+                <li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </li>
+            </ul>
+        </div>
+    </div>
+</div>`;
+
+const detailplate = `
+
+`;
+
 export {
   daftarpopuler,
   daftartestimoni,
   allkuliner,
   searchkuliner,
+  aboutplate,
+  detailplate,
 };
