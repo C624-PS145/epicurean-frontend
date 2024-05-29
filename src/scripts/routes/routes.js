6d2441eeaa71d62d95385586e540a85d9f25abf2
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-05-29 16:34:27 +0700

    routes

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,10 +1,14 @@
 import Pagehome from '../views/pages/pagehome';
 import Pagekuliner from '../views/pages/pagekuliner';
+import Pageabout from '../views/pages/pageabout';
+import Detail from '../views/pages/pagedetail';
 
 const routes = {
   '/': Pagehome, // sebagai halaman utama/ default
   '/pagehome': Pagehome,
   '/pagekuliner': Pagekuliner,
+  '/pageabout': Pageabout,
+  '/detail/:id': Detail,
 };
 
 export default routes;
