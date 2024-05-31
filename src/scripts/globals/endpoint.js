414de60e19e6617b191dd784dc4b4db387026e14
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-05-31 21:37:35 +0700

    new enndpoint search

--- src/scripts/globals/endpoint.js
+++ src/scripts/globals/endpoint.js
@@ -4,6 +4,7 @@ const API_ENDPOINT = {
 
   POPULER: `${CONFIG.BASE_URL}populer`,
   GET_DETAIL_BYID: (id) => `${CONFIG.BASE_URL}detailwisatakuliner/${id}`,
+  SEARCH_FILTER: (query, kabupaten) => `${CONFIG.BASE_URL}search?q=${query}&kabupaten=${kabupaten}`,
 };
 
 export default API_ENDPOINT;
