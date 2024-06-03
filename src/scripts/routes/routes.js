83d511a05a258dd6f238c06e9f90b452e85396e1
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-06-03 10:24:54 +0700

    pageabout

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -2,12 +2,14 @@ import Pagehome from '../views/pages/pagehome';
 import Pagekuliner from '../views/pages/pagekuliner';
 import Pagefavorit from '../views/pages/pagefavorit';
 import Detail from '../views/pages/pagedetail';
+import Pageabout from '../views/pages/pageabout';
 
 const routes = {
   '/': Pagehome, // sebagai halaman utama/ default
   '/pagehome': Pagehome,
   '/pagekuliner': Pagekuliner,
   '/pagefavorit': Pagefavorit,
+  '/pageabout': Pageabout,
   '/detail/:id': Detail,
 };
 
