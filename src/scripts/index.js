6b8063c6fba07ea21ae00a561ac4880076439a91
YohanpermanaRepository <yohandhikristantara99@gmail.com>
2024-06-03 10:49:07 +0700

    smooth hash

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -18,11 +18,54 @@ const app = new App({
   skip: document.querySelector('#skip'),
 });
 
+// Fungsi untuk menggulir halus ke elemen tujuan berdasarkan ID
+const smoothScrollToElement = (hash) => {
+  const id = hash.replace('#', '');
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
 window.addEventListener('hashchange', () => {
   app.renderPage();
+  // Panggil fungsi pengguliran halus setelah halaman dirender
+  setTimeout(() => {
+    smoothScrollToElement(window.location.hash);
+  }, 100); // Timeout untuk memastikan halaman telah dirender
 });
 
 window.addEventListener('load', async () => {
-  app.renderPage();
-  // await swRegister();
+  await app.renderPage();
+  // Panggil fungsi pengguliran halus pada load jika ada hash
+  smoothScrollToElement(window.location.hash);
 });
+
+// import 'regenerator-runtime';
+// import '../styles/home.scss';
+// import '../styles/kuliner.scss';
+// import '../styles/footer.scss';
+// import '../styles/about.scss';
+// import '../styles/detail.scss';
+// import '../styles/bookmark.scss';
+// // eslint-disable-next-line import/extensions
+// import App from './views/app';
+// // import swRegister from './utils/sw-register';
+// // eslint-disable-next-line import/extensions
+// import './component/jumbotron.js';
+
+// const app = new App({
+//   button: document.querySelector('#menuToggle'),
+//   drawer: document.querySelector('#drawer-navigasi'),
+//   content: document.querySelector('main'),
+//   skip: document.querySelector('#skip'),
+// });
+
+// window.addEventListener('hashchange', () => {
+//   app.renderPage();
+// });
+
+// window.addEventListener('load', async () => {
+//   app.renderPage();
+//   // await swRegister();
+// });
